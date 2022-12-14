a280aeb36b2be6c57c148d9335426e5bdf9a44b0
dRumata <rumata@ya.ru>
2022-12-14 17:10:01 +0500

    new file:   mod05/mod05-prep.sh

---
+++ mod05/mod05-prep.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+touch /home/sa/techmint.txt
+touch /home/sa/Techmint.txt
+touch /tmp/.hidden_file
+curl wttr.in > login.php
+curl cheat.sh/find > index.php
+mkdir Techmint
+hostnamectl > techmint.php
+dd if=/dev/zero of=file1.mp3 bs=1M count=55
+dd if=/dev/zero of=file3.mp3 bs=1M count=5
+dd if=/dev/zero of=file2.mp3 bs=10M count=15
+sudo adduser tech
+sudo groupadd devops
+sudo touch /home/techmint.txt
+sudo touch /tmp/techmint.txt
+sudo mkdir /home/devdir
+sudo chown tech:devops /home/techmint.txt
+sudo chown tech:devops /home/devdir
+sudo chmod 777 login.php
+touch /tmp/empty.file
+mkdir /tmp/empty.dir
