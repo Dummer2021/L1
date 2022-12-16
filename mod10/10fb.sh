560ea6b4d9a1a91462cf9269572577bc636b4368
Paul Voitelev <github@voitelev.info>
2022-12-16 14:14:30 +0500

    Create 10fb.sh

---
+++ mod10/10fb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ 
+function isDivisibleBy {
+    return $(($1%$2))
+}
+ 
+function fizzOrBuzz {
+    output=""
+    if isDivisibleBy $1 3; then
+        output="Fizz"
+    fi
+    if isDivisibleBy $1 5; then
+        output="${output}Buzz"
+    fi
+    if [ -z $output ]; then
+        echo $1
+    else
+        echo $output;
+    fi
+}
+ 
+for number in {1..100}; do
+    fizzOrBuzz $number
+done
