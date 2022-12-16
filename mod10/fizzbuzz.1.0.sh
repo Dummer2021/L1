b8995d3671b2dc2441883679208138a3762453d9
Paul Voitelev <github@voitelev.info>
2022-12-16 14:16:51 +0500

    Create fizzbuzz.1.0.sh

---
+++ mod10/fizzbuzz.1.0.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ 
+function isDivisibleBy {
+    return $(($1%$2))
+}
+ 
+function fizzOrBuzz {
+    output=""
+    isDivisibleBy $1 3 && output="Fizz"
+    isDivisibleBy $1 5 && output="${output}Buzz"
+    if [ -z $output ]; then
+        echo $1
+    else
+        echo $output;
+    fi
+}
+ 
+for number in {1..100}; do
+    echo "-`fizzOrBuzz $number`"
+done
