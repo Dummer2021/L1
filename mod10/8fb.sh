d91ca3f03aabb9ba299b68433f44b2712c855a15
Paul Voitelev <github@voitelev.info>
2022-12-16 14:12:39 +0500

    Create 8fb.sh

---
+++ mod10/8fb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ 
+function isDivisibleBy {
+    return $(($1%$2))
+}
+ 
+for number in {1..100}; do
+    output=""
+    if isDivisibleBy $number 3; then
+        output="Fizz"
+    fi
+    if isDivisibleBy $number 5; then
+        output="${output}Buzz"
+    fi
+    if [ -z $output ]; then
+        echo $number
+    else
+        echo $output;
+    fi
+done
